adc3c152623d0db2d257b519554c6ff06e6e1e06
fathiabdelmalek <abdelmalek.fathi.2001@gmail.com>
2022-08-23 14:41:52 +0100

    build profile page skeleton

---
+++ frontend/src/reducers/profile.js
@@ -0,0 +1,31 @@
+import {GET_PROFILE_SUCCESS, GET_PROFILE_FAIL} from "../types";
+
+const initialState = {
+    loading: true,
+    payload: {
+        full_name: "",
+        job_title: "",
+        phone: "",
+        email: "",
+        image: "",
+    }
+}
+
+export default function profileReducer(state = initialState, action) {
+    const { type, payload } = action;
+    switch (type) {
+        case GET_PROFILE_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                payload,
+            };
+        case GET_PROFILE_FAIL:
+            return {
+                ...state,
+                loading: false,
+            };
+        default:
+            return state;
+    }
+}
---
+++ frontend/src/store.js
@@ -0,0 +1,23 @@
+import { combineReducers, createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import profileReducer from "./reducers/profile";
+// import skillsReducer from "./reducers/skills";
+// import projectsReducer from "./reducers/projects";
+// import projectReducer from "./reducers/project";
+// import contactReducer from "./reducers/contact";
+
+const middleware = [thunk];
+
+const reducer = combineReducers({
+  profile: profileReducer,
+  // skills: skillsReducer,
+  // projects: projectsReducer,
+  // project: projectReducer,
+  // contact: contactReducer,
+});
+
+export default createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(...middleware))
+);

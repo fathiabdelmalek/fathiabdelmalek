527ee6355e8b9f3fcbd3759a3cd97ac187bff452
Fathi Abdelmalek <abdelmalek.fathi.2001@gmail.com>
2022-09-08 23:09:21 +0100

    add projects page style

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,5 +1,41 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faFacebook,
+  faTwitter,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <section className="developer">
+        Made with <span style={{ color: "red" }}>&hearts;</span> by Fathi
+        Abdelmalek
+      </section>
+      <section className="links">
+        <a href="https://github.com/fathiabdelmalek" target="_blank">
+          <i>
+            <FontAwesomeIcon icon={faGithub} size="2x" />
+          </i>
+        </a>
+        <a href="https://facebook.com/fathiabdelmalek01" target="_blank">
+          <i>
+            <FontAwesomeIcon icon={faFacebook} size="2x" />
+          </i>
+        </a>
+        <a href="https://twitter.com/AbdelmalekFathi" target="_blank">
+          <i>
+            <FontAwesomeIcon icon={faTwitter} size="2x" />
+          </i>
+        </a>
+        <a href="https://linkedin.com/in/fathi-abdelmalek" target="_blank">
+          <i>
+            <FontAwesomeIcon icon={faLinkedin} size="2x" />
+          </i>
+        </a>
+      </section>
+    </footer>
+  );
 }
--- frontend/src/components/Projects/Project.js
+++ frontend/src/components/Projects/Project.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
 export default function Project({ project }) {
   return (
-    <React.Fragment>
+    <NavLink to={`/projects/${project.id}`}>
       <section className="image-section">
         <img className="project-image" src={project.image} />
       </section>
       <section className="info-section">
-        <NavLink to={`/projects/${project.id}`}>
-          <h3 className="project-name">{project.name}</h3>
-        </NavLink>
-        <span className="project-likes">{project.likes}</span>
+        <h3 className="project-name">{project.name}</h3>
+        <span className="project-likes">
+          {project.likes + " "}
+          <i style={{ color: "red" }}>
+            <FontAwesomeIcon icon={faHeart} />
+          </i>
+        </span>
       </section>
-    </React.Fragment>
+    </NavLink>
   );
 }
